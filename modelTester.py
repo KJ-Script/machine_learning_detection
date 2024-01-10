2a2cf85832dfb9d7678b9920f0dd4d90b4ec095b
jaddis53 <jayaddisu@gmail.com>
2024-01-11 02:28:12 +0300

    Adding all files

--- modelTester.py
+++ modelTester.py
@@ -1,67 +1,120 @@
-import cv2
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import pickle
+import cv2
+import csv
+import os
 import numpy as np
+import pandas as pd
+import pickle
+
+
+with open('model/body_language_demo.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0)
 
-model_dict = pickle.load(open('./model.p', 'rb'))
-model = model_dict['data']
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
-labels_dict = {0: 'Scissor', 1: 'paper', 2: 'Rock'}
-while True:
-    data_aux = []
-    x_ = []
-    y_ = []
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
 
-    ret, frame = cap.read()
-    H, W, _ = frame.shape
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(frame_rgb)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-             mp_drawing.draw_landmarks(
-                frame,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style()
-                )
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x)
-                data_aux.append(y)
-                x_.append(x)
-                y_.append(y)
+        # 1. Draw face landmarks
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                  )
 
-        x1 = int(min(x_) * W)
-        y1 = int(min(y_) * H)
+        # 2. Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                  )
 
-        x2 = int(max(x_) * W)
-        y2 = int(max(y_) * H)
+        # 3. Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                  )
 
-        prediction = model.predict([np.asarray(data_aux)])
-        prediction_word = labels_dict[int(prediction[0])]
+        # 4. Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+        # Export coordinates
+        try:
+            # Extract Pose landmarks
+            pose = results.pose_landmarks.landmark
+            pose_row = list(
+                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
 
-        print("Word: ", prediction_word)
+            # Extract Face landmarks
+            face = results.face_landmarks.landmark
+            face_row = list(
+                np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
 
+            row = pose_row + face_row
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, prediction_word, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 0), 2)
+            X = pd.DataFrame([row])
+            body_language_class = model.predict(X)[0]
+            body_language_prob = model.predict_proba(X)[0]
+            print(body_language_class, body_language_prob)
 
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+            # Grab ear coords
+            coords = tuple(np.multiply(
+                np.array(
+                    (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
+                     results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
+                , [640, 480]).astype(int))
+
+            cv2.rectangle(image,
+                          (coords[0], coords[1] + 5),
+                          (coords[0] + len(body_language_class) * 20, coords[1] - 30),
+                          (245, 117, 16), -1)
+            cv2.putText(image, body_language_class, coords,
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Get status box
+            cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+
+            # Display Class
+            cv2.putText(image, 'CLASS'
+                        , (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, body_language_class.split(' ')[0]
+                        , (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Display Probability
+            cv2.putText(image, 'PROB'
+                        , (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2))
+                        , (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        except:
+            pass
+
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('x'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
+
+tuple(np.multiply(np.array((results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
+results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y)), [640,480]).astype(int))
